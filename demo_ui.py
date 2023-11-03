642c3554ba2555de7e71014c3a401072eea3017f
tangqy <121102723@qq.com>
2023-11-03 13:38:02 +0800

    first commit

---
+++ demo_ui.py
@@ -0,0 +1,110 @@
+import gradio as gr
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import json
+import requests
+import base64
+import hashlib
+import torch
+import time
+import re
+import tempfile
+import shutil
+
+
+DESCRIPTION = '''<h2 style='text-align: center'> 企业搜索问答demo </h2>'''
+default_chatbox = [("", "Hi, What can I do for you?")]
+role_keys = []
+role_values = []
+with open('./role_template.json') as f:
+    global role_keys,role_values
+    json_data = json.load(f)
+    role_keys = list(json_data.keys())
+    role_values = list(json_data.values())
+
+
+
+
+######gradio component func############
+def update_textbox(key):
+    return role_values[key]
+    
+from func import agent_executor
+def execute_agent(query:str,instruct:str):
+    prompt = instruct + "\n" +query
+    return agent_executor.run(prompt)
+
+
+def generate_file(file_obj):
+    global tmpdir
+    print('临时文件夹地址：{}'.format(tmpdir))
+    print('上传文件的地址：{}'.format(file_obj.name)) # 输出上传后的文件在gradio中保存的绝对地址
+
+    #获取到上传后的文件的绝对路径后，其余的操作就和平常一致了
+
+    # 将文件复制到临时目录中
+    shutil.copy(file_obj.name, tmpdir)
+
+    # 获取上传Gradio的文件名称
+    FileName=os.path.basename(file_obj.name)
+
+    # 获取拷贝在临时目录的新的文件地址
+    NewfilePath=os.path.join(tmpdir,FileName)
+    print(NewfilePath)
+
+    # 打开复制到新路径后的文件
+    with open(NewfilePath, 'rb') as file_obj:
+
+        #在本地打开一个新的文件，并且将上传文件内容写入到新文件
+        outputPath=os.path.join(tmpdir,"New"+FileName)
+        with open(outputPath,'wb') as w:
+            w.write(file_obj.read())
+
+    # 返回新文件的的地址（注意这里）
+    return outputPath
+
+
+def clear_fn(value):
+    return "", default_chatbox
+
+
+def main():
+    gr.close_all()
+    global tmpdir,role_keys
+    with tempfile.TemporaryDirectory(dir='./docs/') as tmpdir:
+        with gr.Blocks(css='style.css') as demo:   
+            gr.Markdown(DESCRIPTION)    
+            with gr.Row():
+                with gr.Column(scale=4.5):
+                    with gr.Group():
+                        input_text = gr.Textbox(label='Input Text', placeholder='Please enter text prompt below and press ENTER.')
+                        with gr.Row():
+                            run_button = gr.Button('Generate')
+                            clear_button = gr.Button('Clear')
+                        with gr.Row():
+                            doc_inputs = gr.components.File(label="上传文件")
+                        with gr.Row():
+                            doc_outputs = gr.components.File(label="下载文件",interactive=False)
+    
+    
+                with gr.Column(scale=5.5):
+                    result_text = gr.components.Chatbot(label='Multi-round conversation History', value=[("", "Hi, what can I do for you?")]).style(height=550)
+            with gr.Row():
+                dropdown = gr.Dropdown(role_keys, value=role_keys[0], type="index")
+                instuct_text = gr.Textbox(json_data[keys[0]],visible=False)
+    
+            ###控件事件handler#####
+            dropdown.change(fn=update_textbox, inputs=dropdown, outputs=instuct_text) 
+            run_button.click(fn=execute_agent,inputs=[input_text,instuct_text],outputs=[input_text, result_text])
+            input_text.submit(fn=execute_agent,inputs=[input_text],outputs=[input_text, result_text])
+            clear_button.click(fn=clear_fn, inputs=clear_button, outputs=[input_text, result_text])
+            doc_inputs.upload(fn=generate_file,inputs=[doc_inputs], outputs=[doc_outputs])
+            
+            print(gr.__version__)
+    
+        demo.queue(concurrency_count=10)
+        demo.launch()
+
+
+if __name__ == '__main__':
+    main()
---
+++ func.py
@@ -0,0 +1,234 @@
+from typing import Optional
+import boto3
+import json
+from botocore.config import Config
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
+from langchain.memory import ConversationBufferMemory
+from langchain.agents.openai_functions_agent.base import OpenAIFunctionsAgent
+from langchain.schema.messages import SystemMessage
+from langchain.prompts import (
+    ChatPromptTemplate,
+    PromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.agents.agent_types import AgentType
+from typing import Optional, Type
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForToolRun,
+    CallbackManagerForChainRun
+)
+from langchain.llms.bedrock import Bedrock
+import os
+from langchain.agents.tools import Tool
+from typing import Optional, List, Any
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.agents import initialize_agent
+from langchain.agents.agent_toolkits import create_retriever_tool
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForRetrieverRun,
+    CallbackManagerForRetrieverRun,
+)
+from langchain.retrievers.document_compressors.base import (
+    BaseDocumentCompressor,
+)
+from langchain.schema import BaseRetriever, Document
+from langchain.utilities import SerpAPIWrapper
+
+class BedrockModelWrapper(Bedrock):
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+        prompt = "\nHuman: \n" + prompt + "\nAssistant:"   ## Satisfy Bedrock-Claude prompt requirements
+        return super()._call(prompt, stop, run_manager, **kwargs)
+
+class FullContentRetriever(BaseRetriever):
+    
+    doc_type="txt"
+    doc_path="./"
+    def _get_content_type(self,root_path:str):
+        fullname=""
+        for root, dirs, files in os.walk(root_path):
+            for f in files:
+              fullname = os.path.join(root, f)
+              if "txt" in fullname:
+                    self.doc_type="txt"
+              if "pdf" in fullname:
+                    self.doc_type="pdf"
+        self.doc_path = fullname
+        
+    
+    def _get_relevant_documents(
+        self,
+        query: str,
+        *,
+        run_manager: CallbackManagerForRetrieverRun,
+        **kwargs: Any,
+    ) -> List[Document]:
+        if self.doc_type == "txt":
+            txt_loader = TextLoader(self.doc_path)
+            txt_document = txt_loader.load()
+            return list(txt_document)        
+        elif self.doc_type == "pdf":
+            pdf_loader = PyPDFLoader(self.doc_path)
+            pdf_document = pdf_loader.load()
+            return list(pdf_document)
+        else:
+            return []
+
+def get_bedrock_client(
+    assumed_role: Optional[str] = None,
+    region: Optional[str] = None,
+    runtime: Optional[bool] = True,
+):
+  
+    if region is None:
+        target_region = os.environ.get("AWS_REGION", os.environ.get("AWS_DEFAULT_REGION"))
+    else:
+        target_region = region
+
+    print(f"Create new client\n  Using region: {target_region}")
+    session_kwargs = {"region_name": target_region}
+    client_kwargs = {**session_kwargs}
+
+    profile_name = os.environ.get("AWS_PROFILE")
+    if profile_name:
+        print(f"  Using profile: {profile_name}")
+        session_kwargs["profile_name"] = profile_name
+
+    retry_config = Config(
+        region_name=target_region,
+        retries={
+            "max_attempts": 10,
+            "mode": "standard",
+        },
+    )
+    session = boto3.Session(**session_kwargs)
+
+    if assumed_role:
+        print(f"  Using role: {assumed_role}", end='')
+        sts = session.client("sts")
+        response = sts.assume_role(
+            RoleArn=str(assumed_role),
+            RoleSessionName="langchain-llm-1"
+        )
+        print(" ... successful!")
+        client_kwargs["aws_access_key_id"] = response["Credentials"]["AccessKeyId"]
+        client_kwargs["aws_secret_access_key"] = response["Credentials"]["SecretAccessKey"]
+        client_kwargs["aws_session_token"] = response["Credentials"]["SessionToken"]
+        
+
+    if runtime:
+        service_name='bedrock-runtime'
+    else:
+        service_name='bedrock'
+
+    client_kwargs["aws_access_key_id"] = os.environ.get("AWS_ACCESS_KEY_ID","")
+    client_kwargs["aws_secret_access_key"] = os.environ.get("AWS_SECRET_ACCESS_KEY","")
+    
+    bedrock_client = session.client(
+        service_name=service_name,
+        config=retry_config,
+        **client_kwargs
+    )
+
+    print("boto3 Bedrock client successfully created!")
+    print(bedrock_client._endpoint)
+    return bedrock_client
+
+
+
+## for aksk bedrock
+def get_bedrock_aksk(secret_name='chatbot_bedrock', region_name = "us-west-2"):
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = json.loads(get_secret_value_response['SecretString'])
+    return secret['BEDROCK_ACCESS_KEY'],secret['BEDROCK_SECRET_KEY']
+
+
+ACCESS_KEY, SECRET_KEY=get_bedrock_aksk()
+os.environ["AWS_DEFAULT_REGION"] = "us-west-2"  # E.g. "us-east-1"
+os.environ["AWS_PROFILE"] = "default"
+os.environ["AWS_ACCESS_KEY_ID"]=ACCESS_KEY
+os.environ["AWS_SECRET_ACCESS_KEY"]=SECRET_KEY
+
+
+#新boto3 sdk只能session方式初始化bedrock
+boto3_bedrock = get_bedrock_client(
+    #assumed_role=os.environ.get("BEDROCK_ASSUME_ROLE", None),
+    region=os.environ.get("AWS_DEFAULT_REGION", None)
+)
+
+parameters_bedrock = {
+    "max_tokens_to_sample": 2048,
+    #"temperature": 0.5,
+    "temperature": 0,
+    #"top_k": 250,
+    #"top_p": 1,
+    "stop_sequences": ["\n\nHuman"],
+}
+
+bedrock_llm = Bedrock(model_id="anthropic.claude-v2", client=boto3_bedrock, model_kwargs=parameters_bedrock)
+bedrock_llm_additional = BedrockModelWrapper(model_id="anthropic.claude-v2", 
+                                          client=boto3_bedrock, 
+                                          model_kwargs=parameters_bedrock)
+
+memory = ConversationBufferWindowMemory(k=2)
+system_message = SystemMessage(
+    content=(
+        "Do your best to answer the questions. "
+        "Feel free to use any tools available to look up "
+        "relevant information, only if necessary"
+    )
+)
+prompt = OpenAIFunctionsAgent.create_prompt(
+    system_message=system_message,
+    extra_prompt_messages=[MessagesPlaceholder(variable_name="chat_history")],
+)
+
+retriever = FullContentRetriever()
+retriever._get_content_type("./docs")
+retriever_tool = create_retriever_tool(
+    retriever,
+    "search_enterprise_documents",
+    "useful for when you need to searches and returns documents regarding the user's question",
+)
+search = SerpAPIWrapper()
+search_tool = Tool(
+    name="Search",
+    func=search.run,
+    description="useful for when you need to answer questions by searching the website",
+)
+custom_tool_list = [retriever_tool,search_tool]
+
+agent_executor = initialize_agent(custom_tool_list, bedrock_llm_additional, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, 
+                                  verbose=True,max_iterations=3,
+                                  handle_parsing_errors=True,
+                                  memory=memory,
+                                  return_intermediate_steps=True)
+agent_executor.agent.llm_chain.prompt.template=prompt
